7cc990828c2c8aa8d88a07b0ba4e027367d41956
tanvirs <tanvirs@mkcl.org>
2021-04-22 20:20:18 +0530

    created a custom package calculation inside the calculation folder.

---
+++ calculation/calculation.go
@@ -0,0 +1,14 @@
+package calculation
+
+func Add(number1, number2 int) int {
+	return number1 + number2
+}
+func Substract(number1, number2 int) int {
+	return number1 - number2
+}
+func Multiply(number1, number2 int) int {
+	return number1 * number2
+}
+func Divide(number1, number2 int) int {
+	return number1 / number2
+}
